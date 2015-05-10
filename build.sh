bca61adcf19196a9014d1e8ef3bc187a44030498
Martin Amm <martin.amm@adenin.com>
2015-05-10 19:37:56 -0400

    started cards customization

---
+++ build.sh
@@ -0,0 +1,3 @@
+gulp build
+cp -r dist/carbon /C/Dev/orcharddev/src/Orchard.Web/Modules/CommonLib.Polymer/Components/at-core-theme/dist
+cp -r dist/carbon /C/Dev/designer-0.8-preview/components/at-core-theme/dist
